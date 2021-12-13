d4063e66233d45a0e18dbd368ababd2854b1d108
Eugeny Konstantinov <jskonst@yandex.ru>
2021-12-13 19:19:33 +0300

    Add sample project file

--- function.py
+++ function.py
@@ -1,9 +1,15 @@
-def table(n: int, m: int):
-    print(f"{n + m}")
-    pass
-
+def table(n=9, m=9):
+    n += 1
+    m += 1
+    for i in range(1,n):
+        for j in range(1,m):
+            print(f"{i}x{j}={i*j}")
+        print("-"*10)
 
 if __name__ == "__main__":
-    n = int(input("Please insert base num: "))
-    table(n, 5)
+    n = int(input("Please insert base num n= "))
+    m = int(input("Please insert base num m= "))
+    table(n, m)
+
+    # table() # Call with Default params
     pass
